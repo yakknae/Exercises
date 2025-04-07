fbdf0f1e035c87b014358083f95882596794b60e
gris <168212448+yakknae@users.noreply.github.com>
2025-04-07 14:05:59 -0300

    y-y

--- py/persona.py
+++ py/persona.py
@@ -1,15 +1,108 @@
 #Diseñar un programa en Python que permita gestionar una lista de personas, donde cada persona tiene los siguientes datos: Nombre, Edad, Ciudad
 
-#clase principal persona
+#Clase persona
 class Persona:
     def __init__(self,nombre,edad,ciudad):
-        self.nombre=nombre
+        self.nombre=nombre.strip()
         self.edad= edad
-        self.ciudad = ciudad
+        self.ciudad = ciudad.strip()
 
     def __str__(self):
         return F"nombre: {self.nombre}, edad: {self.edad}, ciudad: {self.ciudad}"
     
+# FUNCION AGREGAR PERSONA
+    @staticmethod
+    def agregar_persona(personas):
+        print("###########################\n")
+        nombre = input("Ingrese el nombre: ").strip()
+        edad = int(input("Ingrese la edad: "))
+        ciudad = input("Ingrese la ciudad: ").strip()
+        nueva_persona = Persona(nombre, edad, ciudad)
+        personas.append(nueva_persona)
+        print(f"{nombre} ha sido agregado/a a la lista.")
+    
+# FUNCION BUSCAR CIUDAD   
+    @staticmethod
+    def buscar_ciudad(personas,ciudad_buscada):
+        print("\n###########################\n")
+        print(f"personas que viven en la ciudad buscada {ciudad_buscada}: ")
+        for person in personas:
+            if ciudad_buscada.lower() == person.ciudad.lower():
+                print(person)
+
+# BUSCAR NOMBRE PERSONA
+    @staticmethod
+    def buscar_nombre(personas,nombre):
+        print("\n###########################\n")
+        print(f"personas que tienen el nombre: {nombre} ")
+        for person in personas:
+            if nombre.lower() == person.nombre.lower():
+                print(person,"\n")
+                
+# FUNCION LISTA PERSONAS
+    @staticmethod
+    def lista_personas(personas):
+        print("\n###########################")
+        print("\nLista actualizada de personas:")
+        for person in personas:
+            print(person)
+
+# FUNCION ASIGNAR MASCOTAS
+    def asignar_mascota(self,mascota):
+        self.mascota = mascota
+        print(f"{self.nombre} es dueño/a ahora de {mascota.nombre}.")
+
+# Clase mascota
+class Mascota:
+    def __init__(self,nombre,edad,tipo):
+        self.nombre = nombre
+        self.edad = edad
+        self.tipo = tipo
+    
+    def __str__(self):
+        return f"nombre: { self.nombre}, edad: {self.edad}, tipo: {self.tipo}"
+    
+# FUNCION LISTA DE MASCOTAS
+    @staticmethod
+    def lista_mascotas(mascotas):
+        print("\n###########################")
+        print("\nLista actualizada de mascotas:")
+        for mascota in mascotas:
+            print(mascota)
+    
+# ASIGNAR MASCOTA A PERSONA
+def asignar_mascota(personas,mascotas):
+    print("\n###########################")
+    print("\nAsignar una mascota a una persona")
+    nombre_persona = input("Ingrese el nombre de la persona: ").strip()
+    nombre_mascota = input("Ingrese el nombre de la mascota: ").strip()
+    
+    # Buscar persona
+    persona_encontrada = None
+    for person in personas:
+        if nombre_persona.lower() == person.nombre.lower():
+            persona_encontrada = person
+            break
+
+    if not persona_encontrada:
+        print(f"No se encontró a ninguna persona con el nombre '{nombre_persona}'.")
+        return
+    
+    # Buscar mascota
+    mascosta_encontrada = None
+    for mascota in mascotas:
+        if nombre_mascota.lower() == mascota.nombre.lower():
+            mascosta_encontrada = mascota
+            break
+    if not mascosta_encontrada:
+        print(f"No se encontró ninguna mascota con el nombre '{nombre_mascota}'.")
+        return
+
+    #Asignar la mascota a la persona
+    persona_encontrada.asignar_mascota(mascosta_encontrada)
+
+
+        
 # Crear una lista de personas
 personas = [
     Persona("Juan",19,"Ituzaingo"),
@@ -17,9 +110,28 @@ personas = [
     Persona("Walter",21,"San Miguel")
 ]
 
+# Crear una lista de mascotas
+mascotas = [
+    Mascota("Firulais", 3, "Perro"),
+    Mascota("Michi", 5, "Gato"),
+    Mascota("Toby", 2, "Perro")
+]
+
+
 # Mostrar las personas que viven en la ciudad buscada
-ciudad_buscada= "Ituzaingo"
-print(f"personas que viven en la ciudad buscada {ciudad_buscada}: ")
-for person in personas:
-    if ciudad_buscada == person.ciudad:
-        print(person)
+Persona.buscar_ciudad(personas,"Ituzaingo");
+
+# Mostrar las personas que tienen el nombre buscado
+Persona.buscar_nombre(personas,"Juan")
+
+# Agregar persona a la lista
+Persona.agregar_persona(personas)
+
+# Mostrar la lista actualizada de personas
+Persona.lista_personas(personas)
+
+# Mostrar la lista actualizada de personas
+Mascota.lista_mascotas(mascotas)
+
+# Asignar mascota a dueño/a
+asignar_mascota(personas,mascotas)
